ef5880fbde3d4352e302e82a07d27466bb9b37f8
Ilya Kostrikov <ikostrikov@gmail.com>
2017-11-11 11:57:28 -0500

    Fix orthogonal init.

--- model.py
+++ model.py
@@ -4,11 +4,39 @@ import torch.nn.functional as F
 from running_stat import ObsNorm
 from distributions import Categorical, DiagGaussian
 
+# A temporary solution from the master branch.
+# https://github.com/pytorch/pytorch/blob/7752fe5d4e50052b3b0bbc9109e599f8157febc0/torch/nn/init.py#L312
+# Remove after the next version of PyTorch gets release.
+def orthogonal(tensor, gain=1):
+    if tensor.ndimension() < 2:
+        raise ValueError("Only tensors with 2 or more dimensions are supported")
+
+    rows = tensor.size(0)
+    cols = tensor[0].numel()
+    flattened = torch.Tensor(rows, cols).normal_(0, 1)
+
+    if rows < cols:
+        flattened.t_()
+
+    # Compute the qr factorization
+    q, r = torch.qr(flattened)
+    # Make Q uniform according to https://arxiv.org/pdf/math-ph/0609050.pdf
+    d = torch.diag(r, 0)
+    ph = d.sign()
+    q *= ph.expand_as(q)
+
+    if rows < cols:
+        q.t_()
+
+    tensor.view_as(q).copy_(q)
+    tensor.mul_(gain)
+    return tensor
+
 
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1 or classname.find('Linear') != -1:
-        nn.init.orthogonal(m.weight.data)
+        orthogonal(m.weight.data)
         if m.bias is not None:
             m.bias.data.fill_(0)
 
