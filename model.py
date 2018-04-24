9d2ad2ec6a5da33005d683b5302a1b11097cf79b
Ilya Kostrikov <ikostrikov@gmail.com>
2018-04-24 14:21:41 -0400

    Cleanup model definitions

--- model.py
+++ model.py
@@ -12,6 +12,11 @@ def weights_init(m):
             m.bias.data.fill_(0)
 
 
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
 class Policy(nn.Module):
     def __init__(self):
         super(Policy, self).__init__()
@@ -53,12 +58,19 @@ class Policy(nn.Module):
 class CNNPolicy(Policy):
     def __init__(self, num_inputs, action_space, use_gru):
         super(CNNPolicy, self).__init__()
-        self.conv1 = nn.Conv2d(num_inputs, 32, 8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
-        self.conv3 = nn.Conv2d(64, 32, 3, stride=1)
-
-        self.linear1 = nn.Linear(32 * 7 * 7, 512)
-
+        
+        self.main = nn.Sequential(
+            nn.Conv2d(num_inputs, 32, 8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 32, 3, stride=1),
+            nn.ReLU(),
+            Flatten(),
+            nn.Linear(32 * 7 * 7, 512),
+            nn.ReLU()
+        )
+        
         if use_gru:
             self.gru = nn.GRUCell(512, 512)
 
@@ -79,11 +91,13 @@ class CNNPolicy(Policy):
     def reset_parameters(self):
         self.apply(weights_init)
 
-        relu_gain = nn.init.calculate_gain('relu')
-        self.conv1.weight.data.mul_(relu_gain)
-        self.conv2.weight.data.mul_(relu_gain)
-        self.conv3.weight.data.mul_(relu_gain)
-        self.linear1.weight.data.mul_(relu_gain)
+        def mult_gain(m):
+            relu_gain = nn.init.calculate_gain('relu')
+            classname = m.__class__.__name__
+            if classname.find('Conv') != -1 or classname.find('Linear') != -1:
+                m.weight.data.mul_(relu_gain)
+    
+        self.main.apply(mult_gain)
 
         if hasattr(self, 'gru'):
             nn.init.orthogonal(self.gru.weight_ih.data)
@@ -95,18 +109,8 @@ class CNNPolicy(Policy):
             self.dist.fc_mean.weight.data.mul_(0.01)
 
     def forward(self, inputs, states, masks):
-        x = self.conv1(inputs / 255.0)
-        x = F.relu(x)
+        x = self.main(inputs / 255.0)
 
-        x = self.conv2(x)
-        x = F.relu(x)
-
-        x = self.conv3(x)
-        x = F.relu(x)
-
-        x = x.view(-1, 32 * 7 * 7)
-        x = self.linear1(x)
-        x = F.relu(x)
 
         if hasattr(self, 'gru'):
             if inputs.size(0) == states.size(0):
@@ -137,11 +141,19 @@ class MLPPolicy(Policy):
 
         self.action_space = action_space
 
-        self.a_fc1 = nn.Linear(num_inputs, 64)
-        self.a_fc2 = nn.Linear(64, 64)
-
-        self.v_fc1 = nn.Linear(num_inputs, 64)
-        self.v_fc2 = nn.Linear(64, 64)
+        self.actor = nn.Sequential(
+            nn.Linear(num_inputs, 64),
+            nn.Tanh(),
+            nn.Linear(64, 64),
+            nn.Tanh()
+        )
+        
+        self.critic = nn.Sequential(
+            nn.Linear(num_inputs, 64),
+            nn.Tanh(),
+            nn.Linear(64, 64),
+            nn.Tanh()
+        )
 
         self.critic_linear = nn.Linear(64, 1)
         self.dist = get_distribution(64, action_space)
@@ -155,29 +167,11 @@ class MLPPolicy(Policy):
 
     def reset_parameters(self):
         self.apply(weights_init_mlp)
-
-        """
-        tanh_gain = nn.init.calculate_gain('tanh')
-        self.a_fc1.weight.data.mul_(tanh_gain)
-        self.a_fc2.weight.data.mul_(tanh_gain)
-        self.v_fc1.weight.data.mul_(tanh_gain)
-        self.v_fc2.weight.data.mul_(tanh_gain)
-        """
-
         if self.dist.__class__.__name__ == "DiagGaussian":
             self.dist.fc_mean.weight.data.mul_(0.01)
-
+    
     def forward(self, inputs, states, masks):
-        x = self.v_fc1(inputs)
-        x = F.tanh(x)
-
-        x = self.v_fc2(x)
-        hidden_critic = F.tanh(x)
-
-        x = self.a_fc1(inputs)
-        x = F.tanh(x)
-
-        x = self.a_fc2(x)
-        hidden_actor = F.tanh(x)
+        hidden_critic = self.critic(inputs)
+        hidden_actor = self.actor(inputs)
 
         return hidden_critic, hidden_actor, states
