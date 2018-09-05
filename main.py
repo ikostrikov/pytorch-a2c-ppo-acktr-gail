65af003602d38cd1f324e0bcfce40c7f0987eec0
Ilya Kostrikov <ikostrikov@gmail.com>
2018-09-05 16:56:06 -0400

    Change rewards logging

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import copy
 import glob
 import os
 import time
+from collections import deque
 
 import gym
 import numpy as np
@@ -79,13 +80,10 @@ def main():
 
     obs = envs.reset()
     rollouts.obs[0].copy_(obs)
-
-    # These variables are used to compute average rewards for all processes.
-    episode_rewards = torch.zeros([args.num_processes, 1])
-    final_rewards = torch.zeros([args.num_processes, 1])
-
     rollouts.to(device)
 
+    episode_rewards = deque(maxlen=10)
+    
     start = time.time()
     for j in range(num_updates):
         for step in range(args.num_steps):
@@ -97,15 +95,14 @@ def main():
                         rollouts.masks[step])
 
             # Obser reward and next obs
-            obs, reward, done, info = envs.step(action)
-            episode_rewards += reward
+            obs, reward, done, infos = envs.step(action)
+
+            for info in infos:
+                if 'episode' in info.keys():
+                    episode_rewards.append(info['episode']['r'])
 
             # If done then clean the history of observations.
             masks = torch.FloatTensor([[0.0] if done_ else [1.0] for done_ in done])
-            final_rewards *= masks
-            final_rewards += (1 - masks) * episode_rewards
-            episode_rewards *= masks
-
             rollouts.insert(obs, recurrent_hidden_states, action, action_log_prob, value, reward, masks)
 
         with torch.no_grad():
@@ -139,13 +136,14 @@ def main():
         if j % args.log_interval == 0:
             end = time.time()
             total_num_steps = (j + 1) * args.num_processes * args.num_steps
-            print("Updates {}, num timesteps {}, FPS {}, mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}, entropy {:.5f}, value loss {:.5f}, policy loss {:.5f}".
+            print("Updates {}, num timesteps {}, FPS {} \n Last {} episodes: mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}, entropy {:.5f}, value loss {:.5f}, policy loss {:.5f}\n".
                 format(j, total_num_steps,
                        int(total_num_steps / (end - start)),
-                       final_rewards.mean(),
-                       final_rewards.median(),
-                       final_rewards.min(),
-                       final_rewards.max(), dist_entropy,
+                       len(episode_rewards),
+                       np.mean(episode_rewards),
+                       np.median(episode_rewards),
+                       np.min(episode_rewards),
+                       np.max(episode_rewards), dist_entropy,
                        value_loss, action_loss))
         if args.vis and j % args.vis_interval == 0:
             try:
