492d312d629e6ef0e29b6a49a68b26c0896e2e3d
Tim Meinhardt <meinhardt.tim@gmail.com>
2018-11-28 18:47:57 +0100

    Add Bernoulli distribution

--- distributions.py
+++ distributions.py
@@ -10,17 +10,25 @@ from utils import AddBias, init
 Modify standard PyTorch distributions so they are compatible with this code.
 """
 
+#
+# Standardize distribution interfaces
+#
+
+# Categorical
 FixedCategorical = torch.distributions.Categorical
 
 old_sample = FixedCategorical.sample
 FixedCategorical.sample = lambda self: old_sample(self).unsqueeze(-1)
 
 log_prob_cat = FixedCategorical.log_prob
-FixedCategorical.log_probs = lambda self, actions: log_prob_cat(self, actions.squeeze(-1)).unsqueeze(-1)
+FixedCategorical.log_probs = lambda self, actions: log_prob_cat(self, actions.squeeze(-1)).view(actions.size(0), -1).sum(-1).unsqueeze(-1)
 
-FixedCategorical.mode = lambda self: self.probs.argmax(dim=1, keepdim=True)
+FixedCategorical.mode = lambda self: self.probs.argmax(dim=-1, keepdim=True)
 
+
+# Normal
 FixedNormal = torch.distributions.Normal
+
 log_prob_normal = FixedNormal.log_prob
 FixedNormal.log_probs = lambda self, actions: log_prob_normal(self, actions).sum(-1, keepdim=True)
 
@@ -30,14 +38,25 @@ FixedNormal.entropy = lambda self: entropy(self).sum(-1)
 FixedNormal.mode = lambda self: self.mean
 
 
+# Bernoulli
+FixedBernoulli = torch.distributions.Bernoulli
+
+log_prob_bernoulli = FixedBernoulli.log_prob
+FixedBernoulli.log_probs = lambda self, actions: log_prob_bernoulli(self, actions).view(actions.size(0), -1).sum(-1).unsqueeze(-1)
+
+entropy = FixedBernoulli.entropy
+FixedBernoulli.entropy = lambda self: entropy(self).sum(-1)
+FixedBernoulli.mode = lambda self: torch.gt(self.probs, 0.5).float()
+
+
 class Categorical(nn.Module):
     def __init__(self, num_inputs, num_outputs):
         super(Categorical, self).__init__()
 
         init_ = lambda m: init(m,
-              nn.init.orthogonal_,
-              lambda x: nn.init.constant_(x, 0),
-              gain=0.01)
+            nn.init.orthogonal_,
+            lambda x: nn.init.constant_(x, 0),
+            gain=0.01)
 
         self.linear = init_(nn.Linear(num_inputs, num_outputs))
 
@@ -51,8 +70,8 @@ class DiagGaussian(nn.Module):
         super(DiagGaussian, self).__init__()
 
         init_ = lambda m: init(m,
-              nn.init.orthogonal_,
-              lambda x: nn.init.constant_(x, 0))
+            nn.init.orthogonal_,
+            lambda x: nn.init.constant_(x, 0))
 
         self.fc_mean = init_(nn.Linear(num_inputs, num_outputs))
         self.logstd = AddBias(torch.zeros(num_outputs))
@@ -67,3 +86,18 @@ class DiagGaussian(nn.Module):
 
         action_logstd = self.logstd(zeros)
         return FixedNormal(action_mean, action_logstd.exp())
+
+
+class Bernoulli(nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super(Bernoulli, self).__init__()
+
+        init_ = lambda m: init(m,
+            nn.init.orthogonal_,
+            lambda x: nn.init.constant_(x, 0))
+
+        self.linear = init_(nn.Linear(num_inputs, num_outputs))
+
+    def forward(self, x):
+        x = self.linear(x)
+        return FixedBernoulli(logits=x)
