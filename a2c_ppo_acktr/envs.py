bc3a68fd2127754f81a1cf3f93ffc81b08667ce3
Ilya Kostrikov <ikostrikov@gmail.com>
2019-01-06 18:07:20 -0500

    Fix a performance issue with non deterministic cuda

--- a2c_ppo_acktr/envs.py
+++ a2c_ppo_acktr/envs.py
@@ -210,7 +210,10 @@ class VecPyTorchFrameStack(VecEnvWrapper):
 
     def reset(self):
         obs = self.venv.reset()
-        self.stacked_obs = torch.zeros(self.stacked_obs.shape)
+        if torch.backends.cudnn.deterministic:
+            self.stacked_obs = torch.zeros(self.stacked_obs.shape)
+        else:
+            self.stacked_obs.zero_()
         self.stacked_obs[:, -self.shape_dim0:] = obs
         return self.stacked_obs
 
