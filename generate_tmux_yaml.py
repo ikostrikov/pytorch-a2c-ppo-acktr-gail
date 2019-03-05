31339f1e2502afabaaa97f3dff0c4d2a041c950a
Ilya Kostrikov <ikostrikov@gmail.com>
2019-03-05 17:50:16 -0500

    Add a template for atari

--- generate_tmux_yaml.py
+++ generate_tmux_yaml.py
@@ -8,14 +8,18 @@ parser.add_argument('--env-names', default="Reacher-v2;HalfCheetah-v2;Walker2d-v
                     help='environment name separated by semicolons')
 args = parser.parse_args()
 
-ppo_template = "python main.py --env-name {0} --algo ppo --use-gae --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --entropy-coef 0 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --tau 0.95 --num-env-steps 1000000 --use-linear-lr-decay --no-cuda --log-dir /tmp/gym/{1}/{1}-{2} --seed {2} --use-proper-time-limits"
+ppo_mujoco_template = "python main.py --env-name {0} --algo ppo --use-gae --log-interval 1 --num-steps 2048 --num-processes 1 --lr 3e-4 --entropy-coef 0 --value-loss-coef 0.5 --ppo-epoch 10 --num-mini-batch 32 --gamma 0.99 --tau 0.95 --num-env-steps 1000000 --use-linear-lr-decay --no-cuda --log-dir /tmp/gym/{1}/{1}-{2} --seed {2} --use-proper-time-limits"
+
+ppo_atari_template = "env CUDA_VISIBLE_DEVICES={2} python main.py --env-name {0} --algo ppo --use-gae --lr 2.5e-4 --clip-param 0.1 --value-loss-coef 0.5 --num-processes 8 --num-steps 128 --num-mini-batch 4 --log-interval 1 --use-linear-lr-decay --use-linear-clip-decay --entropy-coef 0.01 --log-dir /tmp/gym/{1}/{1}-{2} --seed {2}"
+
+template = ppo_atari_template
 
 config = {"session_name": "run-all", "windows": []}
 
 for i in range(args.num_seeds):
     panes_list = []
     for env_name in args.env_names.split(';'):
-        panes_list.append(ppo_template.format(env_name, env_name.split('-')[0].lower(), i))
+        panes_list.append(template.format(env_name, env_name.split('-')[0].lower(), i))
         
     config["windows"].append({"window_name": "seed-{}".format(i), "panes": panes_list})
 
