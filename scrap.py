fef81adb97a3bd8cb54627fa4c83d60d73b66359
roggirg <roger.girgis@gmail.com>
2019-07-05 18:13:22 -0400

    Added scrap script, a non-working version of enjoy for navi.

---
+++ scrap.py
@@ -0,0 +1,107 @@
+import argparse
+import os
+# workaround to unpickle olf model files
+import sys
+
+import numpy as np
+import torch
+
+from a2c_ppo_acktr.envs import VecPyTorch, make_vec_envs
+from a2c_ppo_acktr.utils import get_render_func, get_vec_normalize
+
+sys.path.append('a2c_ppo_acktr')
+
+parser = argparse.ArgumentParser(description='RL')
+parser.add_argument(
+    '--seed', type=int, default=1, help='random seed (default: 1)')
+parser.add_argument(
+    '--log-interval',
+    type=int,
+    default=10,
+    help='log interval, one log per n updates (default: 10)')
+parser.add_argument(
+    '--env-name',
+    default='PongNoFrameskip-v4',
+    help='environment to train on (default: PongNoFrameskip-v4)')
+parser.add_argument(
+    '--load-dir',
+    default='./trained_models/',
+    help='directory to save agent logs (default: ./trained_models/)')
+parser.add_argument(
+    '--non-det',
+    action='store_true',
+    default=False,
+    help='whether to use a non-deterministic policy')
+parser.add_argument(
+    '--custom-gym',
+    default='',
+    help='import some dependency package for thew gym env')
+parser.add_argument(
+    '--navi',
+    action='store_true',
+    default=False,
+    help='use the custom dictionary observation from the navi project')
+args = parser.parse_args()
+
+args.det = not args.non_det
+
+env = make_vec_envs(
+    args.env_name,
+    args.seed + 1000,
+    1,
+    None,
+    None,
+    device='cpu',
+    allow_early_resets=False,
+    custom_gym=args.custom_gym,
+    navi=args.navi
+)
+
+# Get a render function
+render_func = get_render_func(env)
+
+# We need to use the same statistics for normalization as used in training
+actor_critic, ob_rms = torch.load(os.path.join(args.load_dir, args.env_name + ".pt"))
+
+vec_norm = get_vec_normalize(env)
+if vec_norm is not None:
+    vec_norm.eval()
+    vec_norm.ob_rms = ob_rms
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+recurrent_hidden_states = torch.zeros(1,
+                                      actor_critic.recurrent_hidden_state_size).to(device)
+masks = torch.zeros(1, 1).to(device)
+
+obs = env.reset()
+
+if render_func is not None:
+    render_func('human')
+
+if args.env_name.find('Bullet') > -1:
+    import pybullet as p
+
+    torsoId = -1
+    for i in range(p.getNumBodies()):
+        if (p.getBodyInfo(i)[0].decode() == "torso"):
+            torsoId = i
+
+while True:
+    with torch.no_grad():
+        value, action, _, recurrent_hidden_states = actor_critic.act(
+            obs.to(device), recurrent_hidden_states, masks, deterministic=args.det)
+
+    # Obser reward and next obs
+    obs, reward, done, _ = env.step(action)
+
+    masks.fill_(0.0 if done else 1.0)
+
+    if args.env_name.find('Bullet') > -1:
+        if torsoId > -1:
+            distance = 5
+            yaw = 0
+            humanPos, humanOrn = p.getBasePositionAndOrientation(torsoId)
+            p.resetDebugVisualizerCamera(distance, yaw, -20, humanPos)
+
+    if render_func is not None:
+        render_func('human')
