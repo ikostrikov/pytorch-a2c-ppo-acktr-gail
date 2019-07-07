19650d068581c0d685cc532e2eb39f76dba856d4
roggirg <roger.girgis@gmail.com>
2019-07-07 19:20:51 -0400

    Modified comet script.

--- parse_comet.py
+++ parse_comet.py
@@ -16,17 +16,18 @@ exp_ids = {
     "Hyrule-Mini-NoImg-Shaped-v1-s1-10p": "mweiss17/navi-corl-2019/2da12b34c1eb4f4c9f2cdd6f0ec867af",
     "Hyrule-Mini-NoGPS-Shaped-v1-s0-10p": "mweiss17/navi-corl-2019/44daa65b9fde4336a0049a9efce7610c",
     "Hyrule-Mini-NoGPS-Shaped-v1-s1-10p": "mweiss17/navi-corl-2019/fcaf64ee63ab4fa19c58e0578e70f1b4",
+    "Hyrule-Mini-ImgOnly-Shaped-v1-s6-10p": "mweiss17/navi-corl-2019/2b960c6b3db24e6fb859c71f71a5a58b",
+    "Hyrule-Mini-ImgOnly-Shaped-v1-s7-10p": "mweiss17/navi-corl-2019/5dfab8d58a524ab3a443d1a8d46e9d99",
 }
 
-colors = {
-    "Hyrule-Mini-All-Shaped-v1": '#000c1a',
-    "Hyrule-Mini-NoImg-Shaped-v1": '#6b0000',
-    "Hyrule-Mini-NoGPS-Shaped-v1": '#0d8c00',
-    "Hyrule-Mini-ImgOnly-Shaped-v1": '#000cda',
-    "Hyrule-Mini-Pacman-v1": '#e23f3a'
+plot_info = {
+    "Hyrule-Mini-All-Shaped-v1": {'color': '#22a784', 'plot_name': 'AllObs'},
+    "Hyrule-Mini-NoImg-Shaped-v1": {'color': '#fde724', 'plot_name': 'NoImg'},
+    "Hyrule-Mini-NoGPS-Shaped-v1": {'color': '#440154', 'plot_name': 'NoGPS'},
+    "Hyrule-Mini-ImgOnly-Shaped-v1": {'color': '#29788e', 'plot_name': 'ImgOnly'},
 }
 
-reported_metrics = ["Reward Mean", "Episodic Success Rate"]  # , "Episode Length Mean",]
+reported_metrics = ["Reward Mean", "Episodic Success Rate" , "Episode Length Mean ",]
 
 
 def build_plot_dict(orig_env_name, raw_tuples, final_data, log_ts):
@@ -37,6 +38,7 @@ def build_plot_dict(orig_env_name, raw_tuples, final_data, log_ts):
 
         # Preprocess for equal log intervals
         data = np.zeros((temp_data.shape[0], len(log_ts)))
+        data[0, :] = log_ts
         for j in range(len(log_ts)):
             index = np.where(temp_data[0] == log_ts[j])[0]
             if index.size == 0:
@@ -46,8 +48,8 @@ def build_plot_dict(orig_env_name, raw_tuples, final_data, log_ts):
         # If same experiment with different seed
         if env_name in final_data:
             final_data[env_name]['n'] += 1
-            new_data = (final_data[env_name]['data'] + data) / final_data[env_name]['n']
-            final_data[env_name]['data'] = new_data
+            data_concat = np.vstack((final_data[env_name]['data'], data[1, :]))
+            final_data[env_name]['data'] = data_concat
 
         # If first experiment with these hyperparams.
         else:
@@ -68,14 +70,13 @@ for name, exp_id in exp_ids.items():
 
     reward_mean = experiment.metrics_raw[reported_metrics[0]]
     episodic_success_rate = experiment.metrics_raw[reported_metrics[1]]
-    # episode_length_mean = np.array(experiment.metrics_raw[reported_metrics[1]])
-    # print(episode_length_mean)
+    episode_length_mean = experiment.metrics_raw[reported_metrics[2]]
     if name == 'Hyrule-Mini-All-Shaped-v1-s0-10p':
         logged_timesteps = np.array([list(x) for x in reward_mean]).transpose()[0]
 
-    # else:
+
     final_data = build_plot_dict(orig_env_name=env_name,
-                                 raw_tuples=[reward_mean, episodic_success_rate],
+                                 raw_tuples=[reward_mean, episodic_success_rate, episode_length_mean],
                                  final_data=final_data,
                                  log_ts=logged_timesteps)
 
@@ -89,7 +90,15 @@ for metric in reported_metrics:
 
     for key, val in final_data.items():
         if metric in key:
-            plt.plot(val['data'][0], val['data'][1], colors[key.replace(' - '+metric, "")], label=key)
+            color = plot_info[key.replace(' - ' + metric, "")]['color']
+            label = plot_info[key.replace(' - ' + metric, "")]['plot_name']
+
+            met_mean = np.mean(val['data'][1:], axis=0)
+            met_std = np.std(val['data'][1:], axis=0)
+
+            plt.fill_between(val['data'][0], met_mean - met_std, met_mean + met_std, alpha=0.1,
+                             color=color)
+            plt.plot(val['data'][0], met_mean, color, label=label)
 
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
     plt.legend()
