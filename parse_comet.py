1f4c801318de300215708331eeb92c5c2df1a208
roggirg <roger.girgis@gmail.com>
2019-07-09 10:47:06 -0400

    Added Pacman plotting. Added noisy GPS plotting.

--- parse_comet.py
+++ parse_comet.py
@@ -155,3 +155,112 @@ for key, val in final_data.items():
     elif val['metric'] == reported_metrics[2]:
         print("Episode Length Mean. Mean of the last 10 log intervals:", np.mean(np.mean(val['data'][1:], axis=0)[-10:]))
     print("------------------------------------------------------------------------------------------------")
+
+
+# GPS data
+# 4 procs only
+gps_exp_ids = {
+    "Hyrule-Mini-All-Shaped-v1-s0-4p": "mweiss17/navi-corl-2019/a887d1de91c0463e94985441bbf8b126",
+    "Hyrule-Mini-NoisyGPS-1-v1-s0-4p": "mweiss17/navi-corl-2019/29e49d4fb9454638acf103825d3d55c1",
+    "Hyrule-Mini-NoisyGPS-5-v1-s0-4p": "mweiss17/navi-corl-2019/2d13c57bf35d49a0ac26ca1ea9fbc928",
+}
+
+plot_info = {
+    "Hyrule-Mini-All-Shaped-v1": {'color': '#22a784', 'plot_name': 'AllObs'},
+    "Hyrule-Mini-NoisyGPS-1-v1": {'color': '#fde724', 'plot_name': 'NoisyGPS-1'},
+    "Hyrule-Mini-NoisyGPS-5-v1": {'color': '#440154', 'plot_name': 'NoisyGPS-5'},
+}
+
+gps_exp_data = {}
+min_numframes = 0000000
+max_numframes = 10000000
+for name, exp_id in gps_exp_ids.items():
+    gps_exp_data[name] = {}
+    experiment = api.get(exp_id)
+
+    reward_mean = experiment.metrics_raw[reported_metrics[0]]
+    reward_arr = np.array(reward_mean).transpose()
+    reward_arr = reward_arr[:, np.where(np.logical_and(reward_arr[0] >= min_numframes,
+                                                       reward_arr[0] < max_numframes))[0]]
+    gps_exp_data[name][reported_metrics[0]] = {
+        'mean': np.mean(reward_arr[1]),
+        'std': np.std(reward_arr[1]),
+        'raw_data': reward_arr
+    }
+
+    episodic_success_rate = experiment.metrics_raw[reported_metrics[1]]
+    ep_succes_rate_arr = np.array(episodic_success_rate).transpose()
+    ep_succes_rate_arr = ep_succes_rate_arr[:, np.where(np.logical_and(ep_succes_rate_arr[0] >= min_numframes,
+                                                                       ep_succes_rate_arr[0] < max_numframes))[0]]
+    gps_exp_data[name][reported_metrics[1]] = {
+        'mean': np.mean(ep_succes_rate_arr[1]),
+        'std': np.std(ep_succes_rate_arr[1]),
+        'raw_data': ep_succes_rate_arr
+    }
+
+    episode_length_mean = experiment.metrics_raw[reported_metrics[2]]
+    ep_length_mean_arr = np.array(episode_length_mean).transpose()
+    ep_length_mean_arr = ep_length_mean_arr[:, np.where(np.logical_and(ep_length_mean_arr[0] >= min_numframes,
+                                                                       ep_length_mean_arr[0] < max_numframes))[0]]
+    gps_exp_data[name][reported_metrics[2]] = {
+        'mean': np.mean(ep_length_mean_arr[1]),
+        'std': np.std(ep_length_mean_arr[1]),
+        'raw_data': ep_length_mean_arr
+    }
+
+# Plotting Statistics
+for metric in reported_metrics:
+    fig = plt.figure()
+    plt.title(metric, fontsize=18)
+    plt.xlabel('Timesteps', fontsize=14)
+    plt.ylabel(metric, fontsize=14)
+
+    for key, val in gps_exp_data.items():
+        color = plot_info[key.replace("-s0-4p", "")]['color']
+        label = plot_info[key.replace("-s0-4p", "")]['plot_name']
+
+        data = val[metric]["raw_data"]
+        plt.plot(running_mean(data[0], 100), running_mean(data[1], 100), color, label=label)
+
+    plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+    plt.legend(fontsize=14)
+    plt.savefig('plots/gps_exp'+metric + ".png")
+
+
+# Pacman
+pacman_exp_ids = {
+    "Hyrule-Pacman-s0-10p": "mweiss17/navi-corl-2019/e0533d2246fd44eba0bb42d5d7568315",
+    "Hyrule-Pacman-s5-10p": "mweiss17/navi-corl-2019/3a4641cbf6234ad5a70ebf47810921d0",
+}
+
+plot_info = {
+    "Hyrule-Pacman-v1": {'color': '#22a784', 'plot_name': 'Pacman'},
+}
+
+for name, exp_id in pacman_exp_ids.items():
+    experiment = api.get(exp_id)
+    mean_rewards = experiment.metrics_raw[reported_metrics[0]]
+
+    reward_arr = np.array(mean_rewards).transpose()
+    if "s5" in name:
+        data = np.vstack((data, reward_arr[1, :]))
+    else:
+        data = reward_arr
+
+color = plot_info["Hyrule-Pacman-v1"]['color']
+label = plot_info["Hyrule-Pacman-v1"]['plot_name']
+
+met_mean = np.mean(data[1:], axis=0)
+met_std = np.std(data[1:], axis=0)
+
+fig = plt.figure()
+plt.title(reported_metrics[0], fontsize=18)
+plt.xlabel('Timesteps', fontsize=14)
+plt.ylabel(reported_metrics[0], fontsize=14)
+plt.fill_between(running_mean(data[0], 100), running_mean(met_mean - met_std, 100),
+                 running_mean(met_mean + met_std, 100), alpha=0.1, facecolor=color)
+plt.plot(running_mean(data[0], 100), running_mean(met_mean, 100), color, label=label)
+
+plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+plt.legend(fontsize=14)
+plt.savefig('plots/pacman_rewards.png')
