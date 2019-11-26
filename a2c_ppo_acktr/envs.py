fa5e26369a8c05be411d8c1a28a56597a630fa42
InspireX96 <xumw1996@gmail.com>
2019-11-26 15:32:15 -0800

    working fetch env

--- a2c_ppo_acktr/envs.py
+++ a2c_ppo_acktr/envs.py
@@ -40,6 +40,10 @@ def make_env(env_id, seed, rank, log_dir, allow_early_resets, renders):
             else:
                 env = gym.make(env_id, renders=True)
 
+        if 'Fetch' in env_id:
+            print('Wrapping Fetch env for compatibility')
+            env = gym.wrappers.FlattenObservation(env)
+
         is_atari = hasattr(gym.envs, 'atari') and isinstance(
             env.unwrapped, gym.envs.atari.atari_env.AtariEnv)
         if is_atari:
