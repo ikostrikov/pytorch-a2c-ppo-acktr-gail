67b0115e15323a728f0b79b08418d82949e056ee
Kiante Brantley <kiante.brantley@gmail.com>
2021-04-15 12:40:59 -0400

    In FixedNormal method, 'entrop' is typed wrong

--- a2c_ppo_acktr/distributions.py
+++ a2c_ppo_acktr/distributions.py
@@ -37,8 +37,8 @@ class FixedNormal(torch.distributions.Normal):
     def log_probs(self, actions):
         return super().log_prob(actions).sum(-1, keepdim=True)
 
-    def entrop(self):
-        return super.entropy().sum(-1)
+    def entropy(self):
+        return super().entropy().sum(-1)
 
     def mode(self):
         return self.mean
