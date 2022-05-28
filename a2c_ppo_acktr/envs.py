2b21e58d405b1d41504758d3288013a28f9bf9d2
Friedrich Yuan <friedrichyuan19990827@gmail.com>
2022-05-28 15:40:23 +0800

    Update: Support the latest Atari environments

--- a2c_ppo_acktr/envs.py
+++ a2c_ppo_acktr/envs.py
@@ -42,7 +42,7 @@ def make_env(env_id, seed, rank, log_dir, allow_early_resets):
             env = gym.make(env_id)
 
         is_atari = hasattr(gym.envs, 'atari') and isinstance(
-            env.unwrapped, gym.envs.atari.atari_env.AtariEnv)
+            env.unwrapped, gym.envs.atari.AtariEnv)
         if is_atari:
             env = NoopResetEnv(env, noop_max=30)
             env = MaxAndSkipEnv(env, skip=4)
@@ -177,7 +177,8 @@ class VecPyTorch(VecEnvWrapper):
         return obs
 
     def step_async(self, actions):
-        if isinstance(actions, torch.LongTensor):
+        if actions.dtype is torch.int64:
+        #if isinstance(actions, torch.LongTensor):
             # Squeeze the dimension for discrete actions
             actions = actions.squeeze(1)
         actions = actions.cpu().numpy()
