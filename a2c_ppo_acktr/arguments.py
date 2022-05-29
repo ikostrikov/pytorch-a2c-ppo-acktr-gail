51f2162e0d8e3d01e6c10feb83455098ea769a75
Friedrich Yuan <friedrichyuan19990827@gmail.com>
2022-05-29 20:40:42 +0800

    Update: Support the state entropy maximization-based exploration

--- a2c_ppo_acktr/arguments.py
+++ a2c_ppo_acktr/arguments.py
@@ -149,6 +149,11 @@ def get_args():
         action='store_true',
         default=False,
         help='use a linear schedule on the learning rate')
+    parser.add_argument(
+        '--use-sem',
+        action='store_true',
+        default=False,
+        help='use state entropy maximization to improve exploration')
     args = parser.parse_args()
 
     args.cuda = not args.no_cuda and torch.cuda.is_available()
